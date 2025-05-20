05fd65c6f9915145b81402eb57108b2eda202f2a
Enesi Wise <enesiwise@gmail.com>
2025-05-20 19:35:04 +0100

    final touches

--- script.js
+++ script.js
@@ -33,13 +33,10 @@ const cardsData = [
 ];
 
 const cardContainer = document.getElementById("card-container");
-cardContainer.innerHTML = "";
 
 cardsData.forEach((card) => {
-  const eachCard = document.createElement("div");
-  eachCard.classList.add("each-card");
-
-  eachCard.innerHTML = `
+  cardContainer.innerHTML += `
+  <div class="each-card">
     <div class="cardImg-div">
         <img src="${card.image}" alt="${card.title}" class="card-img" />
     </div>
@@ -55,9 +52,8 @@ cardsData.forEach((card) => {
                 d="M223,57a58.07,58.07,0,0,0-81.92-.1L128,69.05,114.91,56.86A58,58,0,0,0,33,139l89.35,90.66a8,8,0,0,0,11.4,0L223,139a58,58,0,0,0,0-82Zm-11.35,70.76L128,212.6,44.3,127.68a42,42,0,0,1,59.4-59.4l.2.2,18.65,17.35a8,8,0,0,0,10.9,0L152.1,68.48l.2-.2a42,42,0,1,1,59.36,59.44Z"
             ></path>
       </svg>
-    </div>`;
-
-  cardContainer.appendChild(eachCard);
+    </div> 
+  </div>`;
 });
 
 // Kosi
