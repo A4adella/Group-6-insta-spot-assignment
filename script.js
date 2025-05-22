8da4f82bcfaeb89f5e1af7b4222ea9b0e7712db3
innocent <workinhim@gmail.com>
2025-05-22 15:47:53 +0100

    Updated the Username and Profession to always beging with Capital letter

--- script.js
+++ script.js
@@ -115,11 +115,11 @@ closeImageModal.addEventListener("click", function () {
   imageModal.classList.add("hidden");
 });
 
-imageModal.addEventListener("click", function(e) {
-  if(e.target === imageModal) {
+imageModal.addEventListener("click", function (e) {
+  if (e.target === imageModal) {
     imageModal.classList.add("hidden");
   }
-})
+});
 
 // Ola
 
@@ -131,21 +131,26 @@ function saveChanges() {
   const name = document.getElementById("userName").value;
   const profession = document.getElementById("professionInput").value;
   const imageInput = document.getElementById("imageInput");
-  // Capitalize the first letter of the name and profession
+
+  // Capitalize the first letter of each word
   function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function (txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    return str.replace(/\b\w/g, function (txt) {
+      return txt.toUpperCase();
     });
   }
+  const capitalizedName = toTitleCase(name);
+  const capitalizedProfession = toTitleCase(profession);
 
   // Update the profile name and profession
   if (name) {
-    document.getElementById("profileName").textContent = name;
+    document.getElementById("profileName").textContent = capitalizedName;
   }
 
   if (profession) {
-    document.getElementById("profileProfession").textContent = profession;
+    document.getElementById("profileProfession").textContent =
+      capitalizedProfession;
   }
+
   // Update the profile image
   if (imageInput.files && imageInput.files[0]) {
     const reader = new FileReader();
@@ -161,6 +166,7 @@ function saveChanges() {
 
   // Close the modal
   document.getElementById("modalOverlay").classList.add("hidden");
+
   // Clear the input fields
   document.getElementById("userName").value = "";
   document.getElementById("professionInput").value = "";
